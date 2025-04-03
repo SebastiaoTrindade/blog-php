ce853b68ecc9fea5c7aa44ef4dc44f49df219cb5
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-04-02 22:17:21 -0300

    Configurando o setup do projeto

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+<?php 
+    include_once('helpers/url.php'); 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Tech</title>
-    <link rel="stylesheet" href="css/style.css">
+    
+    <!--CSS-->
+    <link rel="stylesheet" href="<?php $BASE_URL ?>/css/style.css">
+
+    <!--GOOGLE FONTS-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <h1>Blog Tech</h1>
 </body>
 </html>
